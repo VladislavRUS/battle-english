3a0d6359c29b93c4bcc99907b6f545bcf9e6e44a
= <=>
2018-03-03 14:55:53 +0400

    opponents component

---
+++ src/app/components/opponents/opponents.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-opponents',
+  templateUrl: './opponents.component.html'
+})
+export class OpponentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
